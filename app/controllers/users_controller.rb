4b2291c048e2a681e497dab2aec4db4506698b98
Ryan Yep <ryan.yep@gmail.com>
2015-09-25 15:58:42 -0500

    Created index for users controller; renders error if no users in the database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    if users.nil?
+      render json: { error_msg: "There are no users in the system!" }, status: 404
+    else
+      users = User.all
+      render json: users.to_json, status: 200
+    end
+  end
 end
