8c95e5e21f2d98f0a30e4e3f238198549a6f1615
Ryan Yep <ryan.yep@gmail.com>
2015-09-25 10:01:00 -0500

    Added ability to see one specific restaurant and it throws an error if restaurant doesn't exist in database.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,17 @@
 class RestaurantsController < ApplicationController
 
-  def index
+  def index #displays all restaurants
     restaurants = Restaurant.all
     render json: restaurants.to_json, status: 200
+  end
+
+  def show #displays a specific restaurant
+    if Restaurant.exists?(params[:id])
+      restaurant = Restaurant.find(params[:id])
+      render json: restaurant.to_json, status: 200
+    else
+      render json: { error_msg: "There is no restaurant here!", id: params[:id] }.to_json, status: 404
+    end
   end
 
 end
--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,8 @@
 class SpotsController < ApplicationController
-  def index
+
+  def index #displays all the parking spots belonging to a specific restaurant
     spots = Restaurant.find(params[:restaurant_id]).spots
     render json: spots.to_json, status: 200
   end
+  
 end
