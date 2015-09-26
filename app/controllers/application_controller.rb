7750c1d961bc0f2f0f7fc972d1d4a3d36d769968
Ryan Yep <ryan.yep@gmail.com>
2015-09-26 18:51:47 -0500

    Rewrote set cors security in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,47 @@
 class ApplicationController < ActionController::Base
-  skip_before_filter :verify_authenticity_token
 
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
-  def cors_set_access_control_headers
+  after_filter :set_cors
+
+  def set_cors
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
-  def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
+  def options
+    set_cors
+    head :ok
+  end
+  
+  def not_found
+   render json: { message: 'Requested route not found' }, status: 404
+  end
 
-      render :text => '', :content_type => 'text/plain'
-    end
+  # skip_before_filter :verify_authenticity_token
+  #
+  # before_filter :cors_preflight_check
+  # after_filter :cors_set_access_control_headers
+  #
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  #   headers['Access-Control-Max-Age'] = "1728000"
+  # end
+  #
+  # def cors_preflight_check
+  #   if request.method == 'OPTIONS'
+  #     headers['Access-Control-Allow-Origin'] = '*'
+  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+  #     headers['Access-Control-Max-Age'] = '1728000'
+  #
+  #     render :text => '', :content_type => 'text/plain'
+  #   end
   end
 
 
