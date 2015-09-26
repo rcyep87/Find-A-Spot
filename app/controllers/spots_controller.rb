c6bfb2d2e72f9a4226c4944c520c83d95e861519
Ryan Yep <ryan.yep@gmail.com>
2015-09-26 12:53:32 -0500

    Fixed spots_controller update action. It was not saving changes.

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -40,6 +40,7 @@ class SpotsController < ApplicationController
       spot_update = Spot.find(params[:id])
       spot_update.available = params.fetch(:available, spot_update.available)
       spot_update.user_id   = params.fetch(:user_id  , spot_update.user_id) #consider how to do validation of actual user in database
+      spot_update.save
       render json: spot_update.to_json, status: 200
     else
       render json: { error_msg: "You're trying to update a parking spot that doesn't exist in the database!", id: params[:id] }, status: 404
