ba6701f8e48b0954fd8296accd8b5a0bf583d3ad
Ryan Yep <ryan.yep@gmail.com>
2015-09-26 20:33:04 -0500

    updated Application Controller to include options method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def options
+    head :ok
+  end
+
   def not_found
    render json: { message: 'Requested route not found' }, status: 404
   end
